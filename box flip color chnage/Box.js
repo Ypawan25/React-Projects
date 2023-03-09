d8e4a024ac037e9bfcc2a02e7abf9ebde0143f6d
Pawan Kumar Yadav <impawankryadav@gmail.com>
2023-03-09 22:13:01 +0530

    boxes

---
+++ box flip color chnage/Box.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Box(props) {
+    const styles = {
+        backgroundColor: props.on ? "#222222" : "transparent"
+    }
+    
+    return (
+        <div 
+            style={styles} 
+            className="box"
+            onClick={props.toggle}
+        >
+        </div>
+    )
+}
