494db473cc1a0f43cf5a6647ddbbebe1e030ab26
Pawan Kumar Yadav <impawankryadav@gmail.com>
2023-03-09 11:22:11 +0530

    componetent use

---
+++ component use/App.js
@@ -0,0 +1,27 @@
+import React from "react"
+import boxes from "./boxes"
+import Box from "./Box"
+
+export default function App() {
+    const [squares, setSquares] = React.useState(boxes)
+    
+    const squareElements = squares.map(square => (
+        <Box key={square.id} on={square.on} />
+    ))
+    /**
+     * Challenge part 2:
+     * 1. Create a separate component called "Box" and
+     *    replace the `div` above with our <Box /> components
+     * 2. Pass the Box component a prop called `on` with the
+     *    value of the same name from the `boxes` objects
+     * 3. In the Box component, apply dynamic styles to determine
+     *    the backgroundColor of the box. If it's `on`, set the
+     *    backgroundColor to "#222222". If off, set it to "none"
+     */
+    
+    return (
+        <main>
+            {squareElements}
+        </main>
+    )
+}
---
+++ component use/Box.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Box(props) {
+    const styles = {
+        backgroundColor: props.on ? "#222222" : "none"
+    }
+    
+    return (
+        <div style={styles} className="box"></div>
+    )
+}
