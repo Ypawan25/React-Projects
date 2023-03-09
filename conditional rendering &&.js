02177abe5261a436e2ff1c1781a59a406e6909b6
Pawan Kumar Yadav <impawankryadav@gmail.com>
2023-03-09 22:23:57 +0530

    Create conditional rendering &&.js

---
+++ conditional rendering &&.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+export default function Joke(props) {
+    const [isShown, setIsShown] = React.useState(false)
+    /**
+     * Challenge:
+     * - Only display the punchline paragraph if `isShown` is true
+     */
+    function toggleShown(){
+        setIsShown(prevShown => !prevShown)
+    }
+    return (
+        <div>
+            {props.setup && <h3>{props.setup}</h3>}
+            {isShown && <p>{props.punchline}</p>}
+            <button onClick={toggleShown}>Show Punchline</button>
+            <hr />
+        </div>
+    )
+}
+
+const cond1 = false
+const cond2 = false
+if(cond1 && console.log("Hello there")) {
+    // this code will NOT run
+}
