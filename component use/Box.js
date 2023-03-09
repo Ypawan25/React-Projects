ec91a80133cf753b469e3df35285b86aa800236d
Pawan Kumar Yadav <impawankryadav@gmail.com>
2023-03-09 11:36:47 +0530

    updated box

--- component use/Box.js
+++ component use/Box.js
@@ -1,11 +1,27 @@
 import React from "react"
-
+// import boxes from "boxes"
 export default function Box(props) {
+    /**
+     * Challenge: Create state controlling whether
+     * this box is "on" or "off". Use the incoming
+     * `props.on` to determine the initial state.
+     * 
+     * Create an event listener so when the box is clicked,
+     * it toggles from "on" to "off".
+     * 
+     * Goal: clicking each box should toggle it on and off.
+     */
+    
+    const [checkBox,setCheckbox] = React.useState(props.on)
+    // console.log(checkBox)
     const styles = {
-        backgroundColor: props.on ? "#222222" : "none"
+        backgroundColor: checkBox ? "#222222" : "transparent"
+    }
+    function toggle(){
+        setCheckbox(prevCheck => !prevCheck)
     }
     
     return (
-        <div style={styles} className="box"></div>
+        <div style={styles} className="box" onClick={toggle}></div>
     )
 }
