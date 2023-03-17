cedf6795970aeabc4afa1ee6ad84f37b95796716
Pawan Kumar Yadav <impawankryadav@gmail.com>
2023-03-17 12:19:30 +0530

    mapFromarray

---
+++ mapFromarray/App.js
@@ -0,0 +1,39 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
+/*
+Challenge:
+
+- import the array of data from data.js
+- map over the array to create <Card /> components
+- display the array of card components under the navbar
+  (in place of the current <Card /> component)
+
+Note: We haven't styled the group of components yet, so they'll
+still be block elements, stacked vertically. We'll add styling later.
+*/
+
+export default function App() {
+            // <Hero />
+    const cards = data.map(item => {
+        return (
+            <Card 
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })        
+    
+    return (
+        <div>
+            <Navbar />
+            {cards}
+        </div>
+    )
+}
