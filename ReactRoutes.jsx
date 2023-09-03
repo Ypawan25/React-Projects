f28bb19954aee4b83074d663af916c2fe0bd6778
Pawan Kumar Yadav <impawankryadav@gmail.com>
2023-09-03 20:49:04 +0530

    basic route functionalty

---
+++ ReactRoutes.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+function Home() {
+  return (
+    <h1>Hello, React Router!</h1>
+  )
+}
+
+function About() {
+  return (
+    <h1>About page goes here! 🎉</h1>
+  )
+}
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
